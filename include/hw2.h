a090aac834ba28211aea83227538f0cb92a4b6ef
Hikmat Farhat <62365718+hikmatfarhat-ndu@users.noreply.github.com>
2021-03-27 16:32:14 +0200

    Corrected by instructor

--- include/hw2.h
+++ include/hw2.h
@@ -18,8 +18,9 @@
 std::vector<int> stops(std::vector<int>& stations, int distanceToB, int distanceOnFull) {
     std::vector<int> v;
     int d = 0;
-    for (int i = 1; i < stations.size()-1; i++) {
-        if ((stations[i+1] - d > distanceOnFull) || (stations[i + 1] - d == distanceOnFull)) {
+    for (int i = 0; i < stations.size()-1; i++) {
+         if ((stations[i + 1] - d > distanceOnFull) ) {
+
             v.push_back(stations[i]);
             d = stations[i];
         } 
